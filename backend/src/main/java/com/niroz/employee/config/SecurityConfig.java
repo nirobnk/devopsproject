3986f1b2fc3353afd1f9d0a79be1f5cab5ef0ed6
nirobnk <eg245158@engug.ruh.ac.lk>
2026-02-17 23:51:49 +0530

    add new server ipp

--- backend/src/main/java/com/niroz/employee/config/SecurityConfig.java
+++ backend/src/main/java/com/niroz/employee/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://13.214.25.167:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://175.41.184.192:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
